c3ad834280131705ee9b36c4fae2a313125d181a
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2023-02-20 22:32:47 +0000

    Added component components (#6)
    Closes #2

---
+++ tests/ComponentExtensionsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ARiddlestone\PHPStanCakePHP2\Test;
+
+use PHPStan\Testing\TypeInferenceTestCase;
+
+class ComponentExtensionsTest extends TypeInferenceTestCase
+{
+    /**
+     * @return mixed[]
+     */
+    public function dataFileAsserts(): iterable
+    {
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/existing_component_component.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/invalid_component_property.php');
+    }
+
+    /**
+     * @dataProvider dataFileAsserts
+     * @param mixed $args
+     */
+    public function testControllerExtensions(string $assertType, string $file, ...$args): void
+    {
+        $this->assertFileAsserts($assertType, $file, ...$args);
+    }
+
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [
+            __DIR__ . '/data/phpstan.neon',
+        ];
+    }
+}
---
+++ tests/classes/Controller/Component/SecondComponent.php
@@ -0,0 +1,3 @@
+<?php
+
+class SecondComponent extends Component {}
