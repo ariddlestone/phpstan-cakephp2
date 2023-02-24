b13452506f736b5d4b8659ec62975c06436f26b5
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2023-02-24 11:40:24 +0000

    Add support for Shell Models (#20)
    Closes #14

---
+++ tests/ShellExtensionsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ARiddlestone\PHPStanCakePHP2\Test;
+
+use PHPStan\Testing\TypeInferenceTestCase;
+
+class ShellExtensionsTest extends TypeInferenceTestCase
+{
+    /**
+     * @return mixed[]
+     */
+    public function dataFileAsserts(): iterable
+    {
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/existing_shell_model.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/invalid_shell_property.php');
+    }
+
+    /**
+     * @dataProvider dataFileAsserts
+     * @param mixed $args
+     */
+    public function testShellExtensions(string $assertType, string $file, ...$args): void
+    {
+        $this->assertFileAsserts($assertType, $file, ...$args);
+    }
+
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [
+            __DIR__ . '/data/phpstan.neon',
+        ];
+    }
+}
